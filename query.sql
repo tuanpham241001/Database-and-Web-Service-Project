a5de9fc0d84bc8cc469c8d165fee220d83fc26cf
Tuan Pham <tuanpham241001@gmail.com>
2020-10-12 23:50:46 +0200

    Fourth homework added

--- query.sql
+++ query.sql
@@ -4,12 +4,19 @@ SELECT S.sid,
     S.mat_num
 FROM Students S
 ORDER BY S.name;
-
---2. Count the number of available rooms (Aggregate)
-SELECT COUNT(R.availability)
+--2. Left double room in floor that has more than 2 rooms
+SELECT R.rid,
+    D.position,
+    R.floor
 FROM Rooms R
-WHERE R.availability = 1;
-
+    INNER JOIN Double_rooms D ON D.rid = R.rid
+WHERE R.floor IN (
+        SELECT R.floor
+        FROM Rooms R
+        GROUP BY R.floor
+        HAVING COUNT(R.rid) > 1
+    )
+    AND D.position = 0;
 
 --3. Count the number of Managers in each college (Group by and Join)
 SELECT M.cid,
@@ -17,7 +24,6 @@ SELECT M.cid,
 FROM Managers M
     INNER JOIN Colleges C ON M.cid = C.cid
 GROUP BY M.cid;
-
 --4. Students with their room chosen (Join)
 SELECT S.sid,
     S.name,
@@ -26,14 +32,12 @@ SELECT S.sid,
     R.floor
 FROM Students S
     INNER JOIN Rooms R ON S.rid = R.rid;
-
 --5. Students with double room
 SELECT S.sid,
     S.name,
     S.mat_num
 FROM Students S
     INNER JOIN Double_rooms D ON S.rid = D.rid;
-
 --6. RA that manages college Krupp
 SELECT C.cid,
     C.name,
@@ -43,47 +47,40 @@ FROM Managers M
     INNER JOIN RA ON RA.mgid = M.mgid
     INNER JOIN Colleges C ON C.name = 'Krupp'
     AND C.cid = M.cid;
-
 --7. Find the mailbox of student room who lives in floor 2, 3
-SELECT S.sid, S.name, R.floor, R.mailbox
+SELECT S.sid,
+    S.name,
+    R.floor,
+    R.mailbox
 From Rooms R
     INNER JOIN Students S ON R.rid = S.rid
 WHERE R.floor IN (2, 3);
-
 --8. Count students in their colleges
-SELECT 
-    C.name,
+SELECT C.name,
     R.rnumber,
     COUNT(S.sid) AS students_num
 FROM Rooms R
     INNER JOIN Students S ON S.rid = R.rid
     INNER JOIN Colleges C ON R.cid = C.cid
 GROUP BY R.cid;
-
 --9. Students with special needs who live on floor 2 
 SELECT S.sid,
     S.name,
     Sw.sickness
 FROM Students S
-    INNER JOIN Students_with_special_need Sw ON Sw.sid = S.sid 
+    INNER JOIN Students_with_special_need Sw ON Sw.sid = S.sid
     INNER JOIN Rooms R ON R.rid = S.rid
-WHERE R.floor = 2
-
---10. Double Room on the left in each floor that has at least 2 rooms
-SELECT R.rid
+WHERE R.floor = 2;
+--10. The floor has more than 2 rooms
+SELECT R.floor
 FROM Rooms R
-INNER JOIN Double_rooms D ON D.rid = R.rid AND D.position = 0
 GROUP BY R.floor
-HAVING COUNT(*) > 1
-
---10. All colleges
+HAVING COUNT(*) > 3;
+--11. Count the number of available rooms (Aggregate)
+SELECT COUNT(R.availability)
+FROM Rooms R
+WHERE R.availability = 1;
+    
+--12. All colleges
 SELECT *
 FROM Colleges C;
-
-
-
-
-
-
-
-
