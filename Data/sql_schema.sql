bd585b4000d3661ddc7b05f9bfcdb42a3f519c74
Tuan Pham <tuanpham241001@gmail.com>
2020-10-25 22:52:02 +0100

    College insert

--- Data/sql_schema.sql
+++ Data/sql_schema.sql
@@ -3,37 +3,37 @@ CREATE DATABASE IF NOT EXISTS Housing;
 USE Housing;
 
 CREATE TABLE Colleges(
-    cid CHAR(8),
+    cid INT NOT NULL AUTO_INCREMENT,
     name CHAR(12),
     address CHAR(30),
     PRIMARY KEY (cid)
 );
 
 CREATE TABLE Rooms(
-    rid CHAR(8),
+    rid INT NOT NULL AUTO_INCREMENT,
     rnumber CHAR(5),
     floor INT,
     mailbox CHAR(5),
     availability BIT,
-    cid CHAR(8) NOT NULL,       
+    cid INT NOT NULL,       
     PRIMARY KEY (rid), 
     FOREIGN KEY (cid) REFERENCES Colleges(cid)
 );
 
 CREATE TABLE Double_rooms(
-    rid CHAR(8),
+    rid INT NOT NULL,
     position BIT,
     PRIMARY KEY (rid),
     FOREIGN KEY (rid) REFERENCES Rooms(rid)
 );
 
 CREATE TABLE Students(
-    sid CHAR(8),
+    sid INT NOT NULL AUTO_INCREMENT,
     name CHAR(30),
     mat_num CHAR(8),
     birthday CHAR(8),
-    rid CHAR(8),
-    rsid CHAR (8),
+    rid INT,
+    rsid INT,
     PRIMARY KEY (sid),
     FOREIGN KEY (rsid) REFERENCES Students (sid),
     FOREIGN KEY (rid) REFERENCES Rooms(rid)
@@ -42,7 +42,7 @@ CREATE TABLE Students(
 CREATE TABLE Students_with_special_need(
     sickness CHAR(12),
     special_need CHAR(30),
-    sid CHAR(8),
+    sid INT NOT NULL,
     PRIMARY KEY (sid),
     FOREIGN KEY (sid) REFERENCES Students(sid)
 );
@@ -50,11 +50,11 @@ CREATE TABLE Students_with_special_need(
 
 
 CREATE TABLE Managers(
-    mgid CHAR(8),
+    mgid INT NOT NULL AUTO_INCREMENT,
     name CHAR(30),
     age INT,
     contact_num CHAR(12), 
-    cid CHAR(8),
+    cid INT,
     PRIMARY KEY (mgid),
     FOREIGN KEY (cid) REFERENCES Colleges(cid)
 );
@@ -62,14 +62,14 @@ CREATE TABLE Managers(
 
 CREATE TABLE RA(
     availability BIT,
-    mgid CHAR(8),
+    mgid INT NOT NULL,
     PRIMARY KEY (mgid),
     FOREIGN KEY (mgid) REFERENCES Managers(mgid)   
 );
 
 CREATE TABLE RM(
     office_hour CHAR(30),
-    mgid CHAR(8),
+    mgid INT NOT NULL,
     PRIMARY KEY (mgid),
     FOREIGN KEY (mgid) REFERENCES Managers(mgid)
 );
--- Web/public_html/input/insert/colleges.php
+++ Web/public_html/input/insert/colleges.php
@@ -1,22 +1,13 @@
 <?php
 
-	include('config/db_connect.php');
+	include('../../config/db_connect.php');
 
-	$address = $name = $ingredients = '';
-	$errors = array('address' => '', 'name' => '', 'ingredients' => '');
+	$address = $name = '';
+	
+	$errors = array('address' => '', 'name' => '');
 
 	if(isset($_POST['submit'])){
 		
-		// check address
-		if(empty($_POST['address'])){
-			$errors['address'] = 'An address is required';
-		} else{
-			$address = $_POST['address'];
-			// if(!filter_var($address, FILTER_VALIDATE_address)){
-			// 	$errors['address'] = 'address must be a valid address address';
-			// }
-		}
-
 		// check name
 		if(empty($_POST['name'])){
 			$errors['name'] = 'A name is required';
@@ -27,23 +18,37 @@
 			}
 		}
 
+		// check address
+		if(empty($_POST['address'])){
+			$errors['address'] = 'An address is required';
+		} else{
+			$address = $_POST['address'];
+			// if(!filter_var($address, FILTER_VALIDATE_address)){
+			// 	$errors['address'] = 'address must be a valid address address';
+			// }
+			if(!preg_match('/^[a-zA-Z0-9\s]+$/', $name)){
+				$errors['name'] = 'name must be letters and spaces only';
+			}
+		}
+
+	
 		if(array_filter($errors)){
 			//echo 'errors in form';
 		} else {
 			// escape sql chars
 			$address = mysqli_real_escape_string($conn, $_POST['address']);
 			$name = mysqli_real_escape_string($conn, $_POST['name']);
-	
 
 			// create sql
-			$sql = "INSERT INTO Colleges(name,address) VALUES('$name','$address',)";
+			$sql = "INSERT INTO Colleges(name,address) VALUES('$name','$address')";
 
 			// save to db and check
 			if(mysqli_query($conn, $sql)){
 				// success
-				header('Location: success.php');
+				header('Location: ../success.php');
 			} else {
-				echo 'query error: '. mysqli_error($conn);
+				header('Location: ../failure.php');
+				// echo 'query error: '. mysqli_error($conn);
 			}
 
 		}
@@ -55,26 +60,25 @@
 <!DOCTYPE html>
 <html>
 	
-	<?php include('templates/header.php'); ?>
+	<?php include('../../templates/header.php'); ?>
 
 	<section class="container ">
 		<h4 class="">Add a College</h4>
 		<form class="" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-			<label>College Address</label>
-			<input type="text" name="address" value="<?php echo htmlspecialchars($address) ?>">
-			<div class="red-text"><?php echo $errors['address']; ?></div>
 			<label>College Name</label>
 			<input type="text" name="name" value="<?php echo htmlspecialchars($name) ?>">
-			<div class="red-text"><?php echo $errors['name']; ?></div>
-			<label>Ingredients (comma separated)</label>
-			<input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
-			<div class="red-text"><?php echo $errors['ingredients']; ?></div>
-			<div class="center">
-				<input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
+			<div class=""><?php echo $errors['name']; ?></div>
+
+			<label>College Address</label>
+			<input type="text" name="address" value="<?php echo htmlspecialchars($address) ?>">
+			<div class=""><?php echo $errors['address']; ?></div>
+			
+			<div class="">
+				<input type="submit" name="submit" value="Submit" class="">
 			</div>
 		</form>
 	</section>
 
-	<?php include('templates/footer.php'); ?>
+	<?php include('../../templates/footer.php'); ?>
 
 </html>
