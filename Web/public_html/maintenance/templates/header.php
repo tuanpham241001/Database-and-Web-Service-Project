8545ef1cc767a15951a119a765307a597a5fab16
Tuan Pham <tuanpham241001@gmail.com>
2020-10-26 21:56:16 +0100

    HW6 final

--- Web/public_html/maintenance/templates/header.php
+++ Web/public_html/maintenance/templates/header.php
@@ -1,7 +1,7 @@
   <section id="header">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="../../index.php">
-        <img src="../img/saturn_blue.png" width="30" height="30" class="d-inline-block align-top " alt="logo">
+        <img src="../../img/saturn_blue.png" width="30" height="30" class="d-inline-block align-top " alt="logo">
         Saturn
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
